5a2cf83be2e9afbb9566f78d1880fbfc85501f1e
maintt <nttmaii2407@gmail.com>
2024-09-18 13:21:04 +0700

    user: view request list

--- Mock_Frontend/src/app/app.routes.ts
+++ Mock_Frontend/src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { RegisterComponent } from '../components/auth/register/register.componen
 import { AdminManageUserComponent } from '../components/admin/admin-manage-user/admin-manage-user.component';
 import { BookDetailsComponent } from '../components/user/book-details/book-details.component';
 import { WishListComponent } from '../components/user/wish-list/wish-list.component';
+import { RequestsComponent } from '../components/user/requests/requests.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -22,4 +23,5 @@ export const routes: Routes = [
   { path: 'admin/ManageUser', component: AdminManageUserComponent },
   { path: 'book-details', component: BookDetailsComponent },
   { path: 'wish-list', component: WishListComponent },
+  { path: 'requests', component: RequestsComponent },
 ];

f16f0d32357931cd1fdb7871c56e9e7d65d3b9cd
ManhNguyen0801 <132865577+ManhNguyen0801@users.noreply.github.com>
2024-09-19 09:27:20 +0700

    done manage book

--- Mock_Frontend/src/app/app.component.ts
+++ Mock_Frontend/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from '../components/user/home/home.component';
+import { CommonModule } from '@angular/common';
+import { FirebaseModule } from './firebase/firebase.module';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, HomeComponent],
+    imports: [
+        RouterOutlet,
+        HomeComponent,
+        CommonModule, FirebaseModule
+    ],
+
     templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
     title = 'Mock_Frontend';
---
+++ Mock_Frontend/src/environments/firebase-config.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: false,
+
+    firebaseConfig: {
+        apiKey: "AIzaSyA-sGSV4To_JWvPrJSZEU4a0Se32ZntcDk",
+        authDomain: "mockproject-4ba0d.firebaseapp.com",
+        projectId: "mockproject-4ba0d",
+        storageBucket: "mockproject-4ba0d.appspot.com",
+        messagingSenderId: "1080953402290",
+        appId: "1:1080953402290:web:79706228b86a2ae4db2aab",
+        measurementId: "G-GBMCGJBHDD"
+    }
+
+};
